61db4bbc3994e9f0fdef552e8476fdbe4868e6f5
tunahh029-source <tunahh029@gmail.com>
2026-01-11 13:14:32 +0700

    Update db.py

--- db.py
+++ db.py
@@ -2,9 +2,14 @@ from supabase import create_client
 import streamlit as st
 import uuid
 
-supabase = create_client(
-    st.secrets["SUPABASE_URL"],
-    st.secrets["SUPABASE_KEY"]
-)
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
 
-PLAYER_ID = "demo-player"  # sau này đổi thành user login
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+PLAYER_ID = st.session_state.get(
+    "player_id",
+    str(uuid.uuid4())
+)
+st.session_state["player_id"] = PLAYER_ID
+
